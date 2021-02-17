8cadbb798a81978d72a62dddfb8c22262099a725
Itachi Uchiha <solomonaziel9@gmail.com>
2021-02-17 15:18:21 +0000

    Accidental Push

--- Main.java
+++ Main.java
@@ -1,6 +1,24 @@
+import javax.swing.*;
 public class Main {
-  public static void main(String[] args) {
-    Main mainGuess = new Main();
-  
-  }
+    public static void main(String[] args) {
+
+        // Instantiation
+        figureItOut func = new figureItOut();
+
+
+        int computerNumber = (int) (Math.random()*100 + 1);
+        int userAnswer = 0;
+        System.out.println("Scroll Down To see the answer " + computerNumber);
+        int count = 1;
+
+        while (userAnswer != computerNumber)
+        {
+            String response = JOptionPane.showInputDialog(null,
+                    "Enter a NUMBER guess between 1 and 100", "Guessing Game", 3);
+            userAnswer = Integer.parseInt(response);
+            JOptionPane.showMessageDialog(null, figureItOut.figureItOutComputer(userAnswer, computerNumber, count));
+            count++;
+        }
+    }
+
 }
